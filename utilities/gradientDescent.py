8de00d187ae0c46cb6de69e3a301019ec6778f60
mzarow <mzarow@pgs-soft.com>
2019-03-26 15:46:27 +0100

    update GD derivative

--- utilities/gradientDescent.py
+++ utilities/gradientDescent.py
@@ -1,4 +1,3 @@
-
 class GradientDescent:
 
     def __init__(self, params={}):
@@ -6,21 +5,29 @@ class GradientDescent:
         self.theta0 = params['theta0'] if 'theta0' in params and params['theta0'].isdigit() else 0
         self.theta1 = params['theta1'] if 'theta1' in params and params['theta1'].isdigit() else 0
         self.learning_rate = params['learning_rate'] if 'learning_rate' in params and params['learning_rate'].isdigit() else 0.01
+        self.data = params['data'] if 'data' in params else []
+        self.m = len(self.data)
 
-    # compute derivative for function (x + 10)**2
-    def _derivatie(self, x):
-        return 2 * (x + 10)
+    def _derivative(self):
+        theta0_dx = self.learning_rate/self.m*(sum([self.theta0+self.theta1*self.data[i]['x']-self.data[i]['y']] for i in range(self.m)))
+        theta1_dx = self.learning_rate/self.m*(sum([(self.theta0+self.theta1*self.data[i]['x']-self.data[i]['y'])*self.data[i]['x']] for i in range(self.m)))
+        # save
+        self.theta0 = self.theta0 - self.learning_rate * theta0_dx
+        self.theta1 = self.theta1 - self.learning_rate * theta1_dx
 
-    def _compute_each(self, x):
-        if isinstance(x, str):
-            raise ValueError()
-        return x - (self.learning_rate * self._derivatie(x))
+    def calculate(self, starting_t0=None, starting_t1=None):
+        # if starting theta's provided check type and replace
+        if isinstance(starting_t0, float) or isinstance(starting_t0, int):
+            self.theta0 = starting_t0
+        if isinstance(starting_t1, float) or isinstance(starting_t1, int):
+            self.theta0 = starting_t1
 
-    def calculate(self, starting_value):
-        iters = 10000
-        temp = starting_value
-        for i in range(iters):
-            computed = self._compute_each(temp)
-            if temp == computed:
-                return computed
-            temp = computed
+        max_iterations = 10000
+        temp_t0 = self.theta0
+        temp_t1 = self.theta1
+        for i in range(max_iterations):
+            self._derivative()
+            if temp_t0 == self.theta0 and temp_t1 == self.theta1:
+                print('Found at %i iteration', i)
+                return self.theta0, self.theta1
+        return self.theta0, self.theta1
